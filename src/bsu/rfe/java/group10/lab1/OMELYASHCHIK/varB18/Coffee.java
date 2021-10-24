6df9813e608b6053445442fa49cba7ceb9c553f4
Ivan Omelyashchik <cpt.legion.337@gmail.com>
2021-10-24 14:26:03 +0300

    Add class and them methods:Coffee, nutrience, food

---
+++ src/bsu/rfe/java/group10/lab1/OMELYASHCHIK/varB18/Coffee.java
@@ -0,0 +1,38 @@
+package bsu.rfe.java.group10.lab1.OMELYASHCHIK.varB18;
+
+import java.util.Objects;
+
+public class Coffee extends Food {
+    private String aroma;
+    public Coffee( String aroma) {
+        super("Кофе");
+        this.aroma = aroma;
+    }
+
+    public String getAroma() {
+        return aroma;
+    }
+
+    public void setAroma(String aroma) {
+        this.aroma = aroma;
+    }
+
+    @Override
+    public void consume() {
+        System.out.println(this + "выпит");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Coffee coffee = (Coffee) o;
+        return Objects.equals(aroma, coffee.aroma);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "c ароматом '" + aroma.toUpperCase() + "'";
+    }
+}
---
+++ src/bsu/rfe/java/group10/lab1/OMELYASHCHIK/varB18/Nutrience.java
@@ -0,0 +1,4 @@
+package bsu.rfe.java.group10.lab1.OMELYASHCHIK.varB18;
+
+public interface Nutrience {
+}
