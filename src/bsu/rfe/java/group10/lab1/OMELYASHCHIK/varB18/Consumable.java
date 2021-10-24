cee57e212f6595cc832c1dafadfbcd5e8f505da6
Ivan Omelyashchik <cpt.legion.337@gmail.com>
2021-10-24 12:05:37 +0300

    Create project

---
+++ src/bsu/rfe/java/group10/lab1/OMELYASHCHIK/varB18/Consumable.java
@@ -0,0 +1,9 @@
+package bsu.rfe.java.group10.lab1.OMELYASHCHIK.varB18;
+
+public interface Consumable {
+    public abstract void consume();
+
+    
+
+
+}
---
+++ src/bsu/rfe/java/group10/lab1/OMELYASHCHIK/varB18/MainApplication.java
@@ -0,0 +1,8 @@
+package bsu.rfe.java.group10.lab1.OMELYASHCHIK.varB18;
+
+public class MainApplication {
+
+    public static void main(String[] args){
+
+    }
+}
